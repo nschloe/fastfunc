4bd6fbf57e98199e6bee6647c59ec3f220a594af
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-04 11:48:55 +0200

    lint fix

--- fastfunc/__init__.py
+++ fastfunc/__init__.py
@@ -14,6 +14,20 @@ from .__about__ import (
 
 from . import add, subtract, multiply, divide
 
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__maintainer__",
+    "__status__",
+    "add",
+    "subtract",
+    "multiply",
+    "divide",
+]
+
 try:
     import pipdate
 except ImportError:
