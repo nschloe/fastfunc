e19aa00f49159923280f975a72ee1990bab36da2
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-26 12:03:21 +0200

    more templating, less code

--- src/pybind11.cpp
+++ src/pybind11.cpp
@@ -1,12 +1,13 @@
+#include <functional>
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
 namespace py = pybind11;
 
 
-template <typename T>
+template <typename T, typename OP>
 void
-add_at(
+at(
     py::array_t<T> array,
     py::array_t<int64_t> idx,
     py::array_t<T> vals
@@ -25,97 +26,21 @@ add_at(
 
   for (ssize_t k=0; k < r2.shape(0); k++)
     for (ssize_t j=0; j < r1.shape(1); j++)
-      r1(r2(k), j) += r3(k, j);
-
-  return;
-};
-
-
-template <typename T>
-void
-subtract_at(
-    py::array_t<T> array,
-    py::array_t<int64_t> idx,
-    py::array_t<T> vals
-    )
-{
-  auto r1 = array.template mutable_unchecked<2>();
-  auto r2 = idx.unchecked<1>();
-  auto r3 = vals.template unchecked<2>();
-
-  if (r2.shape(0) != r3.shape(0))
-    throw std::runtime_error("Input shapes must match");
-
-  if (r1.shape(1) != r3.shape(1))
-    throw std::runtime_error("Input shapes must match");
-
-  for (ssize_t k=0; k < r2.shape(0); k++)
-    for (ssize_t j=0; j < r1.shape(1); j++)
-      r1(r2(k), j) -= r3(k, j);
-
-  return;
-};
-
-template <typename T>
-void
-multiply_at(
-    py::array_t<T> array,
-    py::array_t<int64_t> idx,
-    py::array_t<T> vals
-    )
-{
-  auto r1 = array.template mutable_unchecked<2>();
-  auto r2 = idx.unchecked<1>();
-  auto r3 = vals.template unchecked<2>();
-
-  if (r2.shape(0) != r3.shape(0))
-    throw std::runtime_error("Input shapes must match");
-
-  if (r1.shape(1) != r3.shape(1))
-    throw std::runtime_error("Input shapes must match");
-
-  for (ssize_t k=0; k < r2.shape(0); k++)
-    for (ssize_t j=0; j < r1.shape(1); j++)
-      r1(r2(k), j) *= r3(k, j);
-
-  return;
-};
-
-template <typename T>
-void
-divide_at(
-    py::array_t<T> array,
-    py::array_t<int64_t> idx,
-    py::array_t<T> vals
-    )
-{
-  auto r1 = array.template mutable_unchecked<2>();
-  auto r2 = idx.unchecked<1>();
-  auto r3 = vals.template unchecked<2>();
-
-  if (r2.shape(0) != r3.shape(0))
-    throw std::runtime_error("Input shapes must match");
-
-  if (r1.shape(1) != r3.shape(1))
-    throw std::runtime_error("Input shapes must match");
-
-  for (ssize_t k=0; k < r2.shape(0); k++)
-    for (ssize_t j=0; j < r1.shape(1); j++)
-      r1(r2(k), j) /= r3(k, j);
+      r1(r2(k), j) = OP()(r1(r2(k), j), r3(k, j));
 
   return;
 };
 
 PYBIND11_MODULE(_fastfunc, m) {
-  m.def("_add_at", &add_at<int64_t>);
-  m.def("_add_at", &add_at<double>);
+  m.def("_add_at", &at<int64_t, std::plus<int64_t>>);
+  m.def("_add_at", &at<double, std::plus<double>>);
 
-  m.def("_subtract_at", &subtract_at<int64_t>);
-  m.def("_subtract_at", &subtract_at<double>);
+  m.def("_subtract_at", &at<int64_t, std::minus<int64_t>>);
+  m.def("_subtract_at", &at<double, std::minus<double>>);
 
-  m.def("_multiply_at", &multiply_at<int64_t>);
-  m.def("_multiply_at", &multiply_at<double>);
+  m.def("_multiply_at", &at<int64_t, std::multiplies<int64_t>>);
+  m.def("_multiply_at", &at<double, std::multiplies<double>>);
 
-  m.def("_divide_at", &divide_at<int64_t>);
-  m.def("_divide_at", &divide_at<double>);
+  m.def("_divide_at", &at<int64_t, std::divides<int64_t>>);
+  m.def("_divide_at", &at<double, std::divides<double>>);
 }
