56d37520d077ff63553d7d141fea494979605985
Nico Schlömer <nico.schloemer@gmail.com>
2018-07-15 20:58:45 +0200

    assert correct shapes

--- fastfunc/helpers.py
+++ fastfunc/helpers.py
@@ -4,6 +4,18 @@ import sys
 
 
 def _operator_at(operator, a, k, vals):
+    assert (
+        k.shape == vals.shape[: len(k.shape)]
+    ), "Shape mismatch! (target.shape = {}, index.shape = {}, values.shape = {}".format(
+        a.shape, k.shape, vals.shape
+    )
+
+    assert (
+        a.shape[1:] == vals.shape[len(k.shape) :]
+    ), "Shape mismatch! (target.shape = {}, index.shape = {}, values.shape = {}".format(
+        a.shape, k.shape, vals.shape
+    )
+
     a_shape = a.shape
 
     # Never copy the target, a.
@@ -14,8 +26,6 @@ def _operator_at(operator, a, k, vals):
     a = a.reshape(a.shape[0], -1)
     idx = k.reshape(-1)
     v = vals.reshape(idx.shape[0], -1)
-    assert a.shape[1] == v.shape[1]
-    assert a.dtype == vals.dtype
 
     operator(a, idx, v)
 
