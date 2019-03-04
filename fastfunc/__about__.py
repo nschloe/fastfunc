22cdc9f1ddf4497e5548e6165de08c6733e15e97
Nico Schlömer <nico.schloemer@gmail.com>
2019-03-04 12:28:01 +0100

    bump year

--- fastfunc/__about__.py
+++ fastfunc/__about__.py
@@ -3,7 +3,7 @@
 
 __author__ = u"Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
-__copyright__ = u"Copyright (c) 2018, {} <{}>".format(__author__, __author_email__)
+__copyright__ = u"Copyright (c) 2018-2019, {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.1.7"
 __maintainer__ = u"Nico Schlömer"
