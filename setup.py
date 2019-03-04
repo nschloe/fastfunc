4156a10176c753bd377dfcaeb5dd77d76fa09bd0
Nico Schlömer <nico.schloemer@gmail.com>
2019-03-04 12:27:19 +0100

    some install fixes from the pybind11 recommendations

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import codecs
 import subprocess
 import sys
 
+import setuptools
+from setuptools.command.build_ext import build_ext
 from setuptools import setup, Extension, find_packages
 
 
@@ -42,21 +44,73 @@ def read(fname):
     return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
 
 
+# <https://github.com/pybind/python_example/blob/master/setup.py>
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on the specified
+    compiler.
+    """
+    import tempfile
+
+    with tempfile.NamedTemporaryFile("w", suffix=".cpp") as f:
+        f.write("int main (int argc, char **argv) { return 0; }")
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+
+# <https://github.com/pybind/python_example/blob/master/setup.py>
+def cpp_flag(compiler):
+    """Return the -std=c++[11/14] compiler flag.
+    The c++14 is prefered over c++11 (when it is available).
+    """
+    if has_flag(compiler, "-std=c++14"):
+        return "-std=c++14"
+    elif has_flag(compiler, "-std=c++11"):
+        return "-std=c++11"
+    else:
+        raise RuntimeError(
+            "Unsupported compiler -- at least C++11 support " "is needed!"
+        )
+
+
+# <https://github.com/pybind/python_example/blob/master/setup.py>
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+
+    c_opts = {"msvc": ["/EHsc"], "unix": []}
+
+    if sys.platform == "darwin":
+        c_opts["unix"] += ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == "unix":
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, "-fvisibility=hidden"):
+                opts.append("-fvisibility=hidden")
+        elif ct == "msvc":
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+
 ext_modules = [
     Extension(
         "_fastfunc",
         ["src/pybind11.cpp"],
         language="c++",
         include_dirs=[
-            # '/usr/include/eigen3/',
             # Path to pybind11 headers
             get_pybind_include(),
             get_pybind_include(user=True),
         ],
-        libraries=[],
-        # -std=c++11 is needed for older compilers (see, e.g.,
-        # <https://github.com/nschloe/optimesh/issues/26>)
-        extra_compile_args=["-std=c++11"],
     )
 ]
 
@@ -65,6 +119,11 @@ setup(
     name="fastfunc",
     packages=find_packages(),
     ext_modules=ext_modules,
+    #
+    # <https://github.com/pybind/python_example/blob/master/setup.py>
+    cmdclass={"build_ext": BuildExt},
+    zip_safe=False,
+    #
     version=about["__version__"],
     author=about["__author__"],
     author_email=about["__author_email__"],
@@ -74,7 +133,7 @@ setup(
     url="https://github.com/nschloe/fastfunc",
     license=about["__license__"],
     platforms="any",
-    install_requires=["numpy", "pybind11"],
+    install_requires=["numpy", "pybind11>=2.2"],
     classifiers=[
         about["__status__"],
         about["__license__"],
