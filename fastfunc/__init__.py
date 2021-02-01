2d30abfddd699b4a86967864c893c04846825a93
Nico Schlömer <nico.schloemer@gmail.com>
2021-02-01 08:37:54 +0100

    init fix

--- fastfunc/__init__.py
+++ fastfunc/__init__.py
@@ -1,22 +1,8 @@
 from . import add, divide, multiply, subtract
-from .__about__ import (
-    __author__,
-    __author_email__,
-    __copyright__,
-    __license__,
-    __maintainer__,
-    __status__,
-    __version__,
-)
+from .__about__ import __version__
 
 __all__ = [
-    "__author__",
-    "__author_email__",
-    "__copyright__",
-    "__license__",
     "__version__",
-    "__maintainer__",
-    "__status__",
     "add",
     "subtract",
     "multiply",
